7dff203022e9934b965524b54fab38595361cfaa
Francesco Fermi <ffermi@altitudo.com>
2019-12-02 18:29:11 +0100

    Add day02

---
+++ Logic/Year2019/Day02.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace Logic.Year2019
+{
+    public class Day02 : ISolvePuzzles
+    {
+        public string GetSolution1(string input)
+        {
+            int[] instructions = input.Split(',').Select(x => int.Parse(x)).ToArray();
+
+#if  RELEASE
+            instructions[1] = 12;
+            instructions[2] = 2;
+#endif
+
+            for (int i = 0; i < instructions.Length; i = i + 4)
+            {
+                var op = (OpCode)instructions[i];
+                
+                if (op.Equals(OpCode.Halt)) break;
+
+                var data = GetRefData(instructions, i);
+                instructions[data.refResult] = GetResult(op, instructions[data.ref1], instructions[data.ref2]);
+            }
+
+            return string.Join(',', instructions);
+
+            (int ref1, int ref2, int refResult) GetRefData(int[] input, int i) => (instructions[i + 1], instructions[i + 2], instructions[i + 3]);
+
+            int GetResult(OpCode op, int a, int b)
+            {
+                switch (op)
+                {
+                    case OpCode.Add:
+                        return a + b;    
+                    case OpCode.Multiply:
+                        return a * b;
+                    case OpCode.Halt:
+                    default:
+                        return -1;
+                }
+            }
+        }
+
+        public string GetSolution2(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal enum OpCode
+    {
+        Add = 1,
+        Multiply = 2,
+        Halt = 99
+    }
+}
---
+++ Test/TestYear2019/TestDay02.cs
@@ -0,0 +1,27 @@
+﻿using Logic.Year2019;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.TestYear2019
+{
+    [TestClass]
+    public class TestDay02 : TestBase<Day02>
+    {
+        [DataTestMethod]
+        [DataRow("1,0,0,0,99", "2,0,0,0,99")]
+        [DataRow("2,3,0,3,99", "2,3,0,6,99")]
+        [DataRow("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [DataRow("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public override void Solution1(string input, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Day.GetSolution1(input));
+        }
+
+        public override void Solution2(string input, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Day.GetSolution2(input));
+        }
+    }
+}
